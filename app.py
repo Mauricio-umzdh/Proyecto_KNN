6241648b3ac72d7c72260904e98f99f4cd3d12a1
Mauricio-umzdh <mauriciovelasque1478@gmail.com>
2026-06-29 22:18:51 -0500

    Correcion del cuadro de Recomendaciones

--- app.py
+++ app.py
@@ -157,7 +157,11 @@ def init_db():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        prediccion=None,
+        recomendaciones=None
+    )
 
 # =========================
 # PREDICCIÓN
